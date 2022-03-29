cd6f30a3310432ae0cc4bd5fe0226f855f9f7226
DozNaka <dozdguide@gmail.com>
2022-03-28 20:22:19 -0400

    defconfig and build_kernel.sh: Unify a217f and a217m using A217FXXS7CUL2 sources

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+# DozNaka's build script
 
-# export CROSS_COMPILE=$HOME/toolchains/gcc/bin/aarch64-linux-android-
-# export CC=$HOME/toolchains/clang/bin/clang
-export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/bin/aarch64-linux-gnu-
-
+clear
 export LLVM=1
 export PATH="$HOME/toolchains/proton-clang/bin:$PATH"
 export CC=clang
 export CROSS_COMPILE=aarch64-linux-gnu-
+export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/bin/aarch64-linux-gnu-
 
 read -p "Clean source? [N] (Y/N): " clean_confirm
 if [[ $clean_confirm == [yY] || $clean_confirm == [yY][eE][sS] ]]; then
@@ -16,6 +15,21 @@ if [[ $clean_confirm == [yY] || $clean_confirm == [yY][eE][sS] ]]; then
 else
     echo "Source will not be cleaned for this build."
 fi
+
+clear
+echo -e "Choose device\n(1) a217m (dm-verity on)\n(2) a217f (dm-verity off):"
+read device_selection
+if [[ $device_selection == 1 ]]; then
+    echo "Selected a217m"
+    $defconfig = a217m_physwizz_defconfig
+elif [[ $device_selection == 2 ]]; then
+    echo "Selected a217f"
+    $defconfig = a217f_physwizz_defconfig
+else
+    echo "Please selected a valid device!"
+    exit
+fi
+clear
 
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
